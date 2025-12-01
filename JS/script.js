4942be45e1e27459d66326b0584be5f8f6c76273
Miichael Ajayi <michaelajayi1995@gmail.com>
2025-12-01 00:34:15 -0500

    function to add DOM element selectors for weather app

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,10 @@
 
 const API_KEY = '3b747fef83d8a893c320157f6fa07393';
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=3b747fef83d8a893c320157f6fa07393';
+
+// Get DOM elements
+const cityInput = document.getElementById('cityInput');
+const searchBtn = document.getElementById('searchBtn');
+const weatherDisplay = document.getElementById('weatherDisplay');
+const errorDiv = document.getElementById('error');
+const loadingDiv = document.getElementById('loading');
